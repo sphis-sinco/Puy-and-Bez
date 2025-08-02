955399a63b0b4977ab38f618513ff625aae72e9e
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-02 10:26:43 -0500

    javascript template strings

--- src/pages/fanart.js
+++ src/pages/fanart.js
@@ -1,19 +1,19 @@
 var fanarts = [
         {
-                name: ["Retis concept"],
-                author: "Sphis_Sinco",
-                path: ["concepts/characters/Retis"],
+                name: [`Retis concept`],
+                author: `Sphis_Sinco`,
+                path: [`concepts/characters/Retis`],
                 width: [300],
-                dates: ["8/1/2025"],
+                dates: [`8/1/2025`],
         },
 ];
 
 var addFanart = function (
-        title = [""],
-        creator = "",
-        location = [""],
+        title = [``],
+        creator = ``,
+        location = [``],
         w = [300],
-        date = [""]
+        date = [``]
 ) {
         fanarts.push({
                 name: title,
@@ -26,57 +26,55 @@ var addFanart = function (
 
 fanarts = [];
 addFanart(
-        ["Chibi Boys", "Gamer Boys"],
-        "H.R.",
-        ["fanart/H.R/chibi", "fanart/H.R/human"],
+        [`Chibi Boys`, `Gamer Boys`],
+        `H.R.`,
+        [`fanart/H.R/chibi`, `fanart/H.R/human`],
         [150],
-        ["8/2/2025", "8/2/2025"]
+        [`8/2/2025`, `8/2/2025`]
 );
 
-var list = document.getElementById("fanart-list");
-if (fanarts.length > 0) list.removeChild(document.getElementById("empty"));
+var list = document.getElementById(`fanart-list`);
+if (fanarts.length > 0) list.removeChild(document.getElementById(`empty`));
 for (let fanart of fanarts) {
-        var fanartstring = "";
+        var fanartstring = ``;
 
         var i = 0;
         for (let name in fanart.name) {
                 fanartstring += fanart.name[i];
 
-                fanartstring += " (";
+                fanartstring += ` (`;
                 if (fanart.dates[i] != null) fanartstring += fanart.dates[i];
-                else fanartstring += "unknown date";
-                fanartstring += ")";
+                else fanartstring += `unknown date`;
+                fanartstring += `)`;
 
-                if (i + 1 < fanart.name.length) fanartstring += ", ";
-                if (i + 1 == fanart.name.length - 1) fanartstring += " and ";
+                if (i + 1 < fanart.name.length) fanartstring += `, `;
+                if (i + 1 == fanart.name.length - 1) fanartstring += ` and `;
 
                 i++;
         }
 
-        if (fanart.author != null) fanartstring += " by " + fanart.author;
+        if (fanart.author != null) fanartstring += ` by ${fanart.author}`;
 
-        var card = document.createElement("li");
-        card.className = "book-card";
+        var card = document.createElement(`li`);
+        card.className = `book-card`;
 
-        var fanartElement = document.createElement("h4");
+        var fanartElement = document.createElement(`h4`);
         fanartElement.innerHTML = fanartstring;
 
         card.appendChild(fanartElement);
 
         var i = 0;
         for (let art in fanart.path) {
-                var artwork = document.createElement("img");
+                var artwork = document.createElement(`img`);
                 artwork.src =
-                        "../../assets/" +
-                        fanart.path[i] +
-                        ".png";
+                        `../../assets/${fanart.path[i]}.png`;
                 artwork.width = 300;
 
                 if (fanart.width[i] != null) artwork.width = fanart.width[i];
                 card.appendChild(artwork);
 
                 if (i + 1 < fanart.name.length)
-                        card.appendChild(document.createElement("br"));
+                        card.appendChild(document.createElement(`br`));
 
                 i++;
         }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,59 +1,57 @@
 var skits = [
         {
                 number: 1,
-                release: "8/1/2025",
-                link: "pages/skit1.html",
+                release: `8/1/2025`,
+                link: `pages/skit1.html`,
         },
 ];
 
-var addSkit = function (num = 0, rel = "", href = "") {
+var addSkit = function (num = 0, rel = ``, href = ``) {
         skits.push({
                 number: num,
                 release: rel,
                 link:
-                        "/" +
-                        href +
-                        ".html",
+                        `/${href}.html`,
         });
 };
 
 skits = [];
-addSkit(1, "8/1/2025", "pages/skits/skit1");
-addSkit(2, "8/2/2025", "pages/skits/skit2");
-addSkit(3, null, "pages/skits/skit3");
-addSkit(4, null, "pages/skits/skit4");
-addSkit(5, null, "pages/skits/skit5");
-addSkit(6, null, "pages/skits/skit6");
+addSkit(1, `8/1/2025`, `pages/skits/skit1`);
+addSkit(2, `8/2/2025`, `pages/skits/skit2`);
+addSkit(3, null, `pages/skits/skit3`);
+addSkit(4, null, `pages/skits/skit4`);
+addSkit(5, null, `pages/skits/skit5`);
+addSkit(6, null, `pages/skits/skit6`);
 
-var list = document.getElementById("book-list");
-var otherPages = document.getElementById("page-list");
+var list = document.getElementById(`book-list`);
+var otherPages = document.getElementById(`page-list`);
 
-var linkButton = document.createElement("a");
-linkButton.innerHTML = "Fanart";
-linkButton.className = "book-card";
-linkButton.href = "pages/fanart.html";
+var linkButton = document.createElement(`a`);
+linkButton.innerHTML = `Fanart`;
+linkButton.className = `book-card`;
+linkButton.href = `pages/fanart.html`;
 otherPages.appendChild(linkButton);
 
 for (let skit of skits) {
-        var skitString = "Skit " + skit.number;
+        var skitString = `Skit ${skit.number}`;
 
-        var card = document.createElement("li");
-        card.className = "book-card";
+        var card = document.createElement(`li`);
+        card.className = `book-card`;
 
-        var skitElement = document.createElement("h4");
+        var skitElement = document.createElement(`h4`);
         skitElement.innerHTML = skitString;
 
         if (skit.release != null) {
-                skitElement.innerHTML += " (" + skit.release + ")";
+                skitElement.innerHTML += ` (${skit.release})`;
         }
         card.appendChild(skitElement);
 
         if (skit.release == null) {
-                var linkButton = document.createElement("button");
-                linkButton.innerHTML = "Coming Eventually";
+                var linkButton = document.createElement(`button`);
+                linkButton.innerHTML = `Coming Eventually`;
         } else {
-                var linkButton = document.createElement("a");
-                linkButton.innerHTML = "Go to " + skitString;
+                var linkButton = document.createElement(`a`);
+                linkButton.innerHTML = `Go to ${skitString}`;
                 linkButton.href = skit.link;
         }
         card.appendChild(linkButton);
