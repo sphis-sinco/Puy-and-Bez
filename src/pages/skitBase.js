0eed57629fdb0d448b3fd1c3b6275fafa209f1b5
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-02 00:04:31 -0500

    ok this isnt working

--- src/pages/skitBase.js
+++ src/pages/skitBase.js
@@ -1,23 +1,39 @@
 export function readSkitJson(skitJsonPath = "pages/skits/skit1") {
-        var returnData = null;
+        const path = "../../" + skitJsonPath + ".json";
 
-        fetch("https://sphis-sinco.github.io/Puy-and-Bez/" + skitJsonPath + ".json")
+        console.info(path);
+        fetch(path)
                 .then((response) => {
                         // Check if the request was successful
                         if (!response.ok) {
-                                throw new Error(
-                                        `File not found`
-                                );
+                                throw new Error(`File not found`);
                         }
                         // Parse the JSON data from the response
                         return response.json();
                 })
                 .then((data) => {
-                        returnData = data;
+                        console.info("worked!");
+                        console.info(data);
+                        return data;
                 })
                 .catch((error) => {
+                        console.info("nop");
                         console.error("Error fetching or parsing JSON:", error);
+                        return null;
                 });
+}
 
-        return returnData;
+export function loadSkitJson(
+        document, data = { content: [{ type: "", content: "", size: 0, path: "" }] }
+) {
+        console.info("lesgo");
+        console.log(data.content);
+        for (let item in data.content) {
+                if (item.type == 'text') {
+                        var text = document.createElement('p');
+                        text.innerText = item.content;
+
+                        document.body.appendChild(text);
+                }
+        }
 }
