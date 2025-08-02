26971ec9fc0039f4c45fc27a53deb212104764b8
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-02 00:05:00 -0500

    Revert "ok this isnt working"
    This reverts commit 0eed57629fdb0d448b3fd1c3b6275fafa209f1b5.

--- src/pages/skitBase.js
+++ src/pages/skitBase.js
@@ -1,39 +1,23 @@
 export function readSkitJson(skitJsonPath = "pages/skits/skit1") {
-        const path = "../../" + skitJsonPath + ".json";
+        var returnData = null;
 
-        console.info(path);
-        fetch(path)
+        fetch("https://sphis-sinco.github.io/Puy-and-Bez/" + skitJsonPath + ".json")
                 .then((response) => {
                         // Check if the request was successful
                         if (!response.ok) {
-                                throw new Error(`File not found`);
+                                throw new Error(
+                                        `File not found`
+                                );
                         }
                         // Parse the JSON data from the response
                         return response.json();
                 })
                 .then((data) => {
-                        console.info("worked!");
-                        console.info(data);
-                        return data;
+                        returnData = data;
                 })
                 .catch((error) => {
-                        console.info("nop");
                         console.error("Error fetching or parsing JSON:", error);
-                        return null;
                 });
-}
 
-export function loadSkitJson(
-        document, data = { content: [{ type: "", content: "", size: 0, path: "" }] }
-) {
-        console.info("lesgo");
-        console.log(data.content);
-        for (let item in data.content) {
-                if (item.type == 'text') {
-                        var text = document.createElement('p');
-                        text.innerText = item.content;
-
-                        document.body.appendChild(text);
-                }
-        }
+        return returnData;
 }
